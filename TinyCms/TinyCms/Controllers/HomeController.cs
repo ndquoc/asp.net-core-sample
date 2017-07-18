9fd1730fed2de40120d187b51202a9b696039740
Quoc Ngo <quoc.ngo@orientsoftware.net>
2017-07-18 18:29:03 +0700

    Add bootstrap and create simple form with validation

--- TinyCms/TinyCms/Controllers/HomeController.cs
+++ TinyCms/TinyCms/Controllers/HomeController.cs
@@ -3,23 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TinyCms.Models;
 
 namespace TinyCms.Controllers
 {
+    [Route("")]
+    [Route("Home")]
     public class HomeController : Controller
     {
-        [Route("")]
+        [HttpGet("")]
         public IActionResult Index()
         {
             ViewBag.Title = "Index";
             return View();
         }
 
-        [Route("Home/About")]
+        [HttpGet("About")]
         public IActionResult AboutUs()
         {
             ViewBag.Title = "About Us";
             return View("About");
+        }
+
+        [HttpGet("Contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Constact";
+            return View();
+        }
+
+        [HttpPost("Contact")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SubmitContact(ContactModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                ViewBag.IsSuccess = true;
+                return View("Contact", new ContactModel());
+            }
+            else
+            {
+                ViewBag.IsSuccess = false;
+                return View("Contact", model);
+            }
         }
     }
 }
---
+++ TinyCms/TinyCms/Models/ContactModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyCms.Models
+{
+    public class ContactModel
+    {
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [Required]
+        public string Email { get; set; }
+
+        [StringLength(maximumLength: 11, MinimumLength = 9)]
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 20)]
+        public string Message { get; set; }
+    }
+}
